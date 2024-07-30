5acb38ed1751a9374111f6ede27844cd4e3e5240
Hyper-RP <rp302084@gmail.com>
2024-07-30 19:09:06 +0530

    new added

---
+++ leetcode/Shuffle_array.java
@@ -0,0 +1,36 @@
+
+/*
+1470. Shuffle the Array.
+    where 2n is length of array
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Shuffle_array {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] arr = {2, 5, 1, 3, 4, 7};//2,3,5,4,1,7
+        int target = 3;
+        int res[]=shuffle(arr, target);
+        System.out.println(Arrays.toString(res));
+
+    }
+
+
+    public static int[] shuffle(int[] nums, int n) {
+        int temp = n;
+        int temparr[] = new int[2 * n];
+        int x[] = new int[n];
+        int y[] = new int[n];
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            x[i] = nums[i];
+            y[i] = nums[temp];
+            temp++;//{2, 5, 1, 3, 4, 7};//2,3,5,4,1,7
+            temparr[j] = x[i];
+            temparr[j + 1] = y[i];
+            j = j + 2;
+        }
+        return temparr;
+    }
+}
