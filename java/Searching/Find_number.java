d85c5456f9788e4c6fca6a63d19ba7013434db19
Hyper-RP <rp302084@gmail.com>
2024-08-04 18:26:59 +0530

    find exits or not is updated

--- java/Searching/Find_number.java
+++ java/Searching/Find_number.java
@@ -4,14 +4,18 @@ public class Find_number {
     public static void main(String[] args) {
         int []arr={1,2,3,4,5};
         int target=4;
-        for (int i = 0; i < arr.length; i++) {
-            if(target==arr[i]){
-                System.out.println("available at index "+i);
-            }
-//            else {
-//                System.out.println("not available");
-//            }
+       int res= linearsearch(arr,target);
+        System.out.println(res);
+    }
+    static int linearsearch(int []num,int target) {
+        if (num.length == 0) {
+            return -1;
         }
+        for (int i = 0; i < num.length; i++) {
+            if (target == num[i])
+                return i;
+        }
+        return -1;
     }
 
 }
