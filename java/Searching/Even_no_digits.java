ff5653ebaf79e083fc29a9ede04b296d132c8d80
Hyper-RP <rp302084@gmail.com>
2024-08-05 15:39:32 +0530

    Even_no_digits is added

---
+++ java/Searching/Even_no_digits.java
@@ -0,0 +1,26 @@
+package Searching;
+//find numbers that have even no.of digits and if it is even return true or false
+public class Even_no_digits {
+    public static void main(String[] args) {
+        boolean res= ischeck(1235);
+        System.out.println(res);
+    }
+    static boolean ischeck(int num){
+
+       if(even(num)%2==0){
+          return true;
+       }
+       return false;
+    }
+    static int even(int n){
+        if(n<0){
+            n=n*-1;
+        }
+        int count=1;
+        for (int i = 0; 0 < n; i++) {
+            n=n/10;
+            count++;
+        }
+        return n;
+    }
+}
