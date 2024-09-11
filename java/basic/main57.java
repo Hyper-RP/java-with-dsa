e371561f288fd5b63baba8b22883a71077e4f456
Hyper-RP <rp302084@gmail.com>
2024-09-11 15:24:45 +0530

    resume

--- java/basic/main57.java
+++ java/basic/main57.java
@@ -6,35 +6,15 @@ import java.util.Scanner;
 public class main57 {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        ArrayList<ArrayList<Integer>>list=new ArrayList<>();
-        for (int i = 0; i < 3; i++){
-            list.add(new ArrayList<>());
-        }
-        for (int i = 0; i < 3; i++) {
-            for (int j=0;j<3;j++) {
-                (list.get(i)).add(input.nextInt());
-            }
-        }
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
         System.out.println(list);
-//        System.out.println(list);
-//        System.out.println(list.remove(1));
-//        System.out.println(list);
-//        System.out.println(list.get(1));
-//        list.add(10);
-//        list.add(11);
-//        list.add(12);
-//        list.add(13);
-//        list.add(14);
-//        list.add(15);
-//        list.add(16);
-//        System.out.println(list);
-//        System.out.println(list.contains(12));
-//        System.out.println(list.get(1));
-//        System.out.println(list.set(0,100));
-//        System.out.println(list);
-//        System.out.println(list.remove(0) );
-//        System.out.println(list);
-//        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        list.set(1,100);
+        System.out.println(list);
+        System.out.println(list.size());
 
     }
 }
