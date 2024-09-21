56578b430891b6b987e12b34562ddf97a6ea00e8
Hyper-RP <rp302084@gmail.com>
2024-09-21 12:58:28 +0530

    selection sort is updated

--- java/Sorting/Selection_sort.java
+++ java/Sorting/Selection_sort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 //write a simple program for selection sort
 public class Selection_sort {
     public static void main(String[] args) {
-        int []arr={4,5,1,2,3};
+        int []arr={1,2,3,4,5};
         int []res=isCheck(arr);
         System.out.println(Arrays.toString(res));
     }
@@ -12,17 +12,20 @@ public class Selection_sort {
     private static int[] isCheck(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int max=isMax(arr,0,arr.length-(i+1));
-            int temp=arr[max];
-            arr[max]=arr[arr.length-(i+1)];
-            arr[arr.length-(i+1)]=temp;
+            if(arr[max]>arr[arr.length-(i+1)]) {
+                int temp = arr[max];
+                arr[max] = arr[arr.length - (i + 1)];
+                arr[arr.length - (i + 1)] = temp;
+            }
         }
         return arr;
     }
 
     private static int isMax(int[] arr, int start, int end) {
         int max=arr[0];
-        int index=0;;
-        for ( int j = start; j < end; j++) {
+        int index=0;
+
+        for ( int j = 1; j <= end; j++) {
             if(max<arr[j]){
                 max=arr[j];
                 index=j;
