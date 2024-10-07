88f174f733a1fe621f93831d6e2993274bb0aa82
Hyper-RP <rp302084@gmail.com>
2024-10-07 13:41:24 +0530

    swap function is added

--- Patterns/src/inbuild_function/swap.java
+++ Patterns/src/inbuild_function/swap.java
@@ -1,5 +1,21 @@
 package inbuild_function;
 
-public class swap {
+import java.util.Arrays;
 
+public class swap {
+    public static void main(String[] args) {
+        int []arr={4,5,6};
+        int first=0;
+        int second=arr.length-1;
+        int []res=isSwap(arr,first,second);
+        System.out.println(Arrays.toString(res));
+    }
+    public static int []isSwap(int []arr,int first,int second){
+        {
+            int temp=arr[first];
+            arr[first]=arr[second];
+            arr[second]=temp;
+        }
+        return arr;
+    }
 }
