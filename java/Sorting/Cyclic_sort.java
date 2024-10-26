5fea5e5e313716764fc063e48845567fb56f0bb9
Hyper-RP <rp302084@gmail.com>
2024-10-27 01:41:41 +0530

    Cyclic sort is updated

--- java/Sorting/Cyclic_sort.java
+++ java/Sorting/Cyclic_sort.java
@@ -4,14 +4,16 @@ import java.util.Arrays;
 
 public class Cyclic_sort {
     public static void main(String[] args) {
-        int []arr={5,4,3,2,1};
-       int res[]= isCyclic(arr);
+        int []arr={4,3,2,7,8,2,3,1};
+        int res[]= isCyclic(arr);
         System.out.println(Arrays.toString(res));
     }
     public static int[]  isCyclic(int []arr){
+        int temp=0;
         for (int i = 0; i < arr.length;) {
-            if(arr[i]<=arr.length&&arr[i]>0) {
-                int temp = arr[i];
+            temp=arr[i];
+            if(arr[i]<=arr.length&&arr[i]>0 &&arr[i]!=arr[temp-1]) {
+                temp = arr[i];
                 arr[i] = arr[temp - 1];
                 arr[temp - 1] = temp;
             }
