3d97417dd98b10d658d592e87e4972367077eba9
Hyper-RP <rp302084@gmail.com>
2024-10-28 06:22:04 +0530

    string palindrome is added

--- java/String/Palindrome.java
+++ java/String/Palindrome.java
@@ -1,2 +1,21 @@
-package String;public class Palindrome {
+package String;
+//string palindrome
+public class Palindrome {
+    public static void main(String[] args) {
+        String str ="a";
+        System.out.println(isPalindrome(str));
+    }
+    public static boolean isPalindrome (String str){
+        if(str==null){
+            return false;
+        }
+        for (int i = 0; i < str.length()/2; i++) {
+            char st=str.charAt(i);
+            char end=str.charAt(str.length()-1-i);
+            if(st!=end){
+               return false;
+            }
+        }
+        return true;
+    }
 }
