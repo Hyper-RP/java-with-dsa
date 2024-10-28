0e404f7dd05a5080d930580484c1d14187ebc23c
Hyper-RP <rp302084@gmail.com>
2024-10-28 14:40:32 +0530

    patterns are modified

--- Patterns/dleftPyramid.java
+++ Patterns/dleftPyramid.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class dleftPyramid {
+public class dleftPyramid {
+    public static void main(String[] args) {
+        pattern(4);
+    }
+
+    private static void pattern(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = n-i; j >= 0; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
 }
--- Patterns/rectangle.java
+++ Patterns/rectangle.java
@@ -1,2 +1,14 @@
 public class rectangle {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    private static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
