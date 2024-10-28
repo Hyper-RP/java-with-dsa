6fd24343e5071b7d5ce229eba1268c2d5905c649
Hyper-RP <rp302084@gmail.com>
2024-10-28 23:18:12 +0530

    pattern 6 is added

---
+++ Patterns/drightPyramid.java
@@ -0,0 +1,19 @@
+public class drightPyramid {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    private static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i<=j){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
