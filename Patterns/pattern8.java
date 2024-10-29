4205692fb6f6c78039ceb9017f5953c17974081e
Hyper-RP <rp302084@gmail.com>
2024-10-29 19:24:44 +0530

    down and up mix pyramid

---
+++ Patterns/pattern8.java
@@ -0,0 +1,30 @@
+public class pattern8 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    private static void pattern(int n) {
+        int total_col=0;
+        int NoOfSpaces=0;
+        for (int i = 0; i <= n*2; i++) {
+            if(i<=n) {
+                NoOfSpaces = n - (n - i);
+            }
+            else {
+            NoOfSpaces=n-(i-n);
+            }
+            for (int j = 0; j < NoOfSpaces; j++) {
+                System.out.print(" ");
+            }
+          if(i<n){
+              total_col=n-i;
+          }
+          else {
+             total_col= i-n;
+          }
+            for (int j = 0; j <= total_col; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
