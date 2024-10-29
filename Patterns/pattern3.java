240171b3a78d45f56530898b46260706796d6488
Hyper-RP <rp302084@gmail.com>
2024-10-29 19:33:25 +0530

    right side up pyramid is added

--- Patterns/pattern3.java
+++ Patterns/pattern3.java
@@ -2,15 +2,15 @@ public class pattern3 {
     public static void main(String[] args) {
         pattern(5);
     }
-    private static void pattern(int n){
 
-        for (int i = 0; i <=n ; i++) {
+    private static void pattern(int n) {
+
+        for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= n; j++) {
 
                 if (n - i <= j) {
                     System.out.print("*");
-                }
-                else {
+                } else {
                     System.out.print(" ");
                 }
 
