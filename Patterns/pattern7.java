d26b7eb7aa26ba5a4f232c4a57b048644e542af0
Hyper-RP <rp302084@gmail.com>
2024-10-29 19:01:30 +0530

    up and down mix pyramid

--- Patterns/pattern7.java
+++ Patterns/pattern7.java
@@ -4,32 +4,32 @@ public class pattern7 {
     }
 
     private static void pattern(int n) {
-        int total_col=0;
-        for (int i = 1; i <= n*2; i++) {
+        int total_col = 0;
+        for (int i = 1; i <= n * 2; i++) {
             //to print column 1 to n
-            if(i<=n) {
+            if (i <= n) {
                 total_col = n;
             }
             //to print column n-1 to
-            else{
-                total_col=n-(i - n);
+            else {
+                total_col = n - (i - n);
             }
-            //to prin spaces
-            int noOfSpace=0;
-            // print spces form n-1 to 1
-            if (i<n) {
-               noOfSpace =n-i;
+            //to print spaces
+            int noOfSpace = 0;
+            // print spaces form n-1 to 1
+            if (i < n) {
+                noOfSpace = n - i;
             }
             //print spaces from 1 to n-1
-            else{
-                noOfSpace=i-n;
+            else {
+                noOfSpace = i - n;
             }
             for (int k = 0; k < noOfSpace; k++) {
                 System.out.print(" ");
             }
             //to print star
-            for (int j = 1; j <=total_col; j++) {
-                if(n-i<j) {
+            for (int j = 1; j <= total_col; j++) {
+                if (n - i < j) {
                     System.out.print("* ");
                 }
             }
