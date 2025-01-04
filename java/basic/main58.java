321b4fe73f573aaa210afc6d15d658b771bd48e4
Hyper-RP <rp302084@gmail.com>
2025-01-04 15:33:43 +0530

    sqrt number

---
+++ java/basic/main58.java
@@ -0,0 +1,40 @@
+package basic;
+
+public class main58 {
+    public static void main(String[] args) {
+        double res= ischeck(5);
+        System.out.println(res);
+    }
+    static double ischeck(int num){
+        double res=0;
+        double start;
+        double end =num;
+        for ( start=1; start <= end;) {
+             double mid = (start+end)/2;
+                res=mid;
+             if(start<end&&start!=mid) {
+                 if (mid * mid == num) {
+                     res = mid;
+                     break;
+                 } else if (end*end == num) {
+                     res=end;
+                     break;
+                 } else if (mid * mid > num) {
+                     end = mid;
+                 } else if (mid * mid < num) {
+                     start = mid ;
+                 }
+             } else if (mid==start) {
+                 if ((mid) * (mid) == num) {
+                     res = mid;
+                     break;
+                 }
+                 else{
+                     mid=mid+0.1;
+                     start=start+0.1;
+                 }
+             }
+        }
+        return res;
+    }
+}
